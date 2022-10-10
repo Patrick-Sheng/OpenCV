2c6746b8f77682cb6338b0d0388528a4c46a50bb
Patrick-Sheng <69024158+Patrick-Sheng@users.noreply.github.com>
2022-10-10 15:45:46 +1300

    Final file with commenting done

--- FINAL - Pose detection.py
+++ FINAL - Pose detection.py
@@ -1,133 +1,181 @@
-# import python packages
-import numpy as np
-import mediapipe as mp
+# Import python packages
 import cv2
+import mediapipe as mp
+import numpy as np
 
-# setting up mediapipe
+from time import sleep
+
+# Set up pose library and drawing util from mediapipe
+mp_pose = mp.solutions.pose
 mp_drawing = mp.solutions.drawing_utils
-mp_holistic = mp.solutions.holistic
 
-# converting image to black and white to save resources while processing
-def mediapipe_detection(image, holistic):
+# Set detection confidence to 0.5 for precise estimation (Note: Lower value for better precision)
+pose_image = mp_pose.Pose(static_image_mode=True, min_detection_confidence=0.5)
+
+
+# Convert image from colour to grey-scale
+def mediapipe_detection(image, pose):
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image.flags.writeable = False
-    # processing image with mediapipe holistic
-    results = holistic.process(image)
-    image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-    return image, results
+    result = pose.process(image)
+    return image, result
 
-# calcating angle for different joints
+
+# Calculate angle from three different coordinates of body parts
 def calculate_angle(landmarks, point):
-    # collecting x-coordinates and y-coordinates
-    if point == "Head":
-        landmark_one = [landmarks[mp_holistic.PoseLandmark.NOSE].x, landmarks[mp_holistic.PoseLandmark.NOSE.value].y]
-        landmark_two = [landmarks[mp_holistic.PoseLandmark.RIGHT_SHOULDER.value].x, landmarks[mp_holistic.PoseLandmark.RIGHT_SHOULDER.value].y]
-        landmark_three = [landmarks[mp_holistic.PoseLandmark.RIGHT_HIP.value].x, landmarks[mp_holistic.PoseLandmark.RIGHT_HIP.value].y]
-    elif point == "Shoulder":
-        landmark_one = [landmarks[mp_holistic.PoseLandmark.RIGHT_SHOULDER].x, landmarks[mp_holistic.PoseLandmark.RIGHT_SHOULDER.value].y]
-        landmark_two = [landmarks[mp_holistic.PoseLandmark.RIGHT_ELBOW.value].x, landmarks[mp_holistic.PoseLandmark.RIGHT_ELBOW.value].y]
-        landmark_three = [landmarks[mp_holistic.PoseLandmark.RIGHT_WRIST.value].x, landmarks[mp_holistic.PoseLandmark.RIGHT_WRIST.value].y]
-    elif point == "Hip":
-        landmark_one = [landmarks[mp_holistic.PoseLandmark.RIGHT_SHOULDER].x, landmarks[mp_holistic.PoseLandmark.RIGHT_SHOULDER.value].y]
-        landmark_two = [landmarks[mp_holistic.PoseLandmark.RIGHT_HIP.value].x, landmarks[mp_holistic.PoseLandmark.RIGHT_HIP.value].y]
-        landmark_three = [landmarks[mp_holistic.PoseLandmark.RIGHT_KNEE.value].x, landmarks[mp_holistic.PoseLandmark.RIGHT_KNEE.value].y]
-    elif point == "Knee":
-        landmark_one = [landmarks[mp_holistic.PoseLandmark.RIGHT_HIP].x, landmarks[mp_holistic.PoseLandmark.RIGHT_HIP.value].y]
-        landmark_two = [landmarks[mp_holistic.PoseLandmark.RIGHT_KNEE.value].x, landmarks[mp_holistic.PoseLandmark.RIGHT_KNEE.value].y]
-        landmark_three = [landmarks[mp_holistic.PoseLandmark.RIGHT_ANKLE.value].x, landmarks[mp_holistic.PoseLandmark.RIGHT_ANKLE.value].y]
 
-    # converting landmarks to numpy arrays
+    # Switch statement to calculate angle for different body parts
+    # Note: landmark one, two, three for three different body part coordinates
+    match point:
+        case "Head":
+            landmark_one = [landmarks[mp_pose.PoseLandmark.NOSE].x, landmarks[mp_pose.PoseLandmark.NOSE.value].y]
+            landmark_two = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x,
+                            landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            landmark_three = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                              landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+        case "Shoulder":
+            landmark_one = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER].x,
+                            landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            landmark_two = [landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].x,
+                            landmarks[mp_pose.PoseLandmark.RIGHT_ELBOW.value].y]
+            landmark_three = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x,
+                              landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+        case "Hip":
+            landmark_one = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER].x,
+                            landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y]
+            landmark_two = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].x,
+                            landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            landmark_three = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+                              landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+        case "Knee":
+            landmark_one = [landmarks[mp_pose.PoseLandmark.RIGHT_HIP].x, landmarks[mp_pose.PoseLandmark.RIGHT_HIP.value].y]
+            landmark_two = [landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].x,
+                            landmarks[mp_pose.PoseLandmark.RIGHT_KNEE.value].y]
+            landmark_three = [landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].x,
+                              landmarks[mp_pose.PoseLandmark.RIGHT_ANKLE.value].y]
+
+    # Convert landmarks into numpy arrays. (Note: [0] is x-coordinate and [1] is y-coordinate)
     arr_landmark_one = np.array(landmark_one)
     arr_landmark_two = np.array(landmark_two)
     arr_landmark_three = np.array(landmark_three)
 
-    # use function to calculate distance between points
+    # Calculate distance between each coordinate
     a = calc_cd_distance(arr_landmark_one, arr_landmark_two)
     b = calc_cd_distance(arr_landmark_two, arr_landmark_three)
     c = calc_cd_distance(arr_landmark_one, arr_landmark_three)
 
-    # calculation to find angle
-    rad_angle = np.arccos((a**2 + b**2 - c**2)/(2*a*b))
-    deg_angle = (180/np.pi) * rad_angle
+    # Calculate angle from distance values, using Cosine rule.
+    rad_angle = np.arccos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b))
+    # Convert angle from radians to degrees
+    deg_angle = (180 / np.pi) * rad_angle
 
     return deg_angle
 
-# function to find distance between two points
+
+# Calculate distance between two coordinates using distance formula
 def calc_cd_distance(arr_one, arr_two):
-    dist = ((arr_one[0] - arr_two[0])**2 + (arr_one[1] - arr_two[1])**2)**0.5
+    # Note: [0] is x-coordinate and [1] is y-coordinate
+    dist = ((arr_one[0] - arr_two[0]) ** 2 + (arr_one[1] - arr_two[1]) ** 2) ** 0.5
     return dist
 
 
-def determine_posture(head_angle, shoulder_angle, hip_angle, knee_angle):
-    # determine whether if the user is sitting in the right posture (angles formed from body parts will fit in a specific range)
+# Determine posture from angles, good body postures will output angle values at a certain range
+def determine_posture(head_angle, shoulder_angle, hip_angle, knee_angle, org_image):
+    # Ouputing Good/Bad results for different body parts, for user to adjust to.
     if 140 < head_angle < 180:
         # image, "Window name", "org, co-ordinate", font, fontScale, colour, thickness, cv2.LINE_AA
-        cv2.putText(image, 'Head: Good angle', (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(org_image, 'Head: Good angle', (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
     else:
-        cv2.putText(image, 'Head: Bad angle', (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(org_image, 'Head: Bad angle', (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
     if 90 < shoulder_angle < 135:
         # image, "Window name", "org, co-ordinate", font, fontScale, colour, thickness, cv2.LINE_AA
-        cv2.putText(image, 'Shoulder: Good angle', (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(org_image, 'Shoulder: Good angle', (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
     else:
-        cv2.putText(image, 'Shoulder: Bad angle', (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(org_image, 'Shoulder: Bad angle', (100, 150), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
     if 90 < hip_angle < 125:
         # image, "Window name", "org, co-ordinate", font, fontScale, colour, thickness, cv2.LINE_AA
-        cv2.putText(image, 'Hip: Good angle', (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(org_image, 'Hip: Good angle', (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
     else:
-        cv2.putText(image, 'Hip: Bad angle', (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(org_image, 'Hip: Bad angle', (100, 200), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
     if 90 < knee_angle < 125:
         # image, "Window name", "org, co-ordinate", font, fontScale, colour, thickness, cv2.LINE_AA
-        cv2.putText(image, 'Knee: Good angle', (100, 250), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+        cv2.putText(org_image, 'Knee: Good angle', (100, 250), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
     else:
-        cv2.putText(image, 'Knee: Bad angle', (100, 250), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
+        cv2.putText(org_image, 'Knee: Bad angle', (100, 250), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
-# setting webcam properties
-wCam, hCam = 1280, 720
-cap = cv2.VideoCapture(0)
-cap.set(3, wCam)
-cap.set(4, hCam)
 
-# tracking image with lower confidence level (slightly under-fitting)
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while cap.isOpened():
+# Main class
+# Detecting pose through camera input
+def detectPose(image, org_image, pose, draw=False, display=False):
 
-        # opening webcam to capture feed
-        ret, frame = cap.read()
-        # preprocess image for calculation purposes
-        image, results = mediapipe_detection(frame, holistic)
+    # Convert image from colour to grey-scale
+    image, result = mediapipe_detection(image, pose)
 
-        # display landmarks with connections on screen for testing/user viewing purposes
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+    # Try to collect landmarks from pose detection, passing landmarks if they can't be seen (i.e. Only half of the body is observed)
+    try:
+        landmarks = result.pose_landmarks.landmark
+    except:
+        pass
+
+    # Collecting angle for each body mark, using landmarks (Require processing)
+    head_angle = calculate_angle(landmarks, "Head")
+    shoulder_angle = calculate_angle(landmarks, "Shoulder")
+    hip_angle = calculate_angle(landmarks, "Hip")
+    knee_angle = calculate_angle(landmarks, "Knee")
+
+    sleep(1)
+
+    # Determine posture from angles, good body postures will output angle values at a certain range
+    determine_posture(head_angle, shoulder_angle, hip_angle, knee_angle, org_image)
+
+    # Show lines of points and joints on user body for display purposes
+    if result.pose_landmarks and draw:
+        mp_drawing.draw_landmarks(org_image, result.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                   mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=2),
                                   mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=3))
 
-        # Use try and except to prevent machine breaking when there's coordinate that cannot be captured/seen
-        try:
-            landmarks = results.pose_landmarks.landmark
-        except:
-            pass
+    cv2.putText(org_image, 'Press any key to exit', (500, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+    # Show processed image with text on window popup
+    cv2.imshow('Image', org_image)
 
-        # calculating angles for different body parts
-        head_angle = calculate_angle(landmarks, "Head")
-        shoulder_angle = calculate_angle(landmarks, "Shoulder")
-        hip_angle = calculate_angle(landmarks, "Hip")
-        knee_angle = calculate_angle(landmarks, "Knee")
-        # print(shoulder_angle)
 
-        # determine posture correction with body parts (no return)
-        determine_posture(head_angle, shoulder_angle, hip_angle, knee_angle)
+# Main code
 
-        # show webcam feed with processed calculation/connections on screen
-        cv2.imshow('Pose estimation', image)
+# Choose camera output device, (0) for first device
+cap = cv2.VideoCapture(0)
+# Set up dimensions for camera output device
+wCam, hCam = 1280, 720
+cap.set(3, wCam)
+cap.set(4, hCam)
 
-        # stop when 'z' key is pressed on keyboard
-        if cv2.waitKey(10) & 0xFF == ord('z'):
-            break
-            
-    # close windows
-    cap.release()
-    cv2.destroyAllWindows()
+while cap.isOpened():
+
+    # Collecting images and videos through camera capture
+    ret, frame = cap.read()
+    cv2.putText(frame, 'Press "z" key to capture image', (500, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+    # Show image with text on window popup
+    cv2.imshow('Pose estimation', frame)
+
+    # Collect data after user presses "z" key
+    if cv2.waitKey(10) & 0xFF == ord('z'):
+
+        # Wait for 3 seconds for user to prepare
+        sleep(3)
+
+        # Make copy of images and videos through camera capture, and write image on image file
+        ret_copy, frame_copy = cap.read()
+        cv2.imwrite("Real_image.png", frame_copy)
+        # Close windows
+        cv2.destroyWindow('Pose estimation')
+        cap.release()
+
+# Open image file and process with pose detection
+file_name = 'Real_image.png'
+output = cv2.imread(file_name)
+detectPose(output, output, pose_image, draw=True, display=True)
+
+# Close window if user presses any key (after results)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
